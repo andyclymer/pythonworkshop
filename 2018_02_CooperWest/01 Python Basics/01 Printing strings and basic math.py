93914e2a570494b1f2564f7ff986500221186637
Andy Clymer <git@andyclymer.com>
2018-02-24 10:46:02 -0800

    Added basics

---
+++ 2018_02_CooperWest/01 Python Basics/01 Printing strings and basic math.py
@@ -0,0 +1,11 @@
+# Strings are in quotes
+print("Hello")
+
+# Numbers are not in quotes...
+print(20*2)
+
+# Can't divide strings and integers
+#print("Hello"/2)
+# But I can add and multiply strings
+print("Hello"*2)
+print("ABC" + "def")
